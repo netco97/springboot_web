868cae22c71b7ff02e0104ec7847d511c1f7d430
isangmin <netco97@naver.com>
2023-01-07 18:18:13 +0900

    API 등록/수정/조회

--- src/main/java/com/example/Spring/domain/posts/Posts.java
+++ src/main/java/com/example/Spring/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.example.Spring.domain.posts;
 
+import com.example.Spring.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,7 +9,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
     @Id //PK(Primary Key)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -25,5 +26,10 @@ public class Posts {
         this.title = title;
         this.content = content;
         this.author = author;
+    }
+
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
     }
 }
---
+++ src/main/java/com/example/Spring/web/PostsApiController.java
@@ -0,0 +1,30 @@
+package com.example.Spring.web;
+
+import com.example.Spring.service.PostsService;
+import com.example.Spring.web.dto.PostsResponseDto;
+import com.example.Spring.web.dto.PostsSaveRequestDto;
+import com.example.Spring.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestsDto){
+        return postsService.save(requestsDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id){
+        return postsService.findById(id);
+    }
+}
