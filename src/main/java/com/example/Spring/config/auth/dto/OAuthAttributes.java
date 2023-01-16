435ed7b91486756f612b90085bb27d58ef6ef745
isangmin <netco97@naver.com>
2023-01-16 21:39:11 +0900

    Naver API

--- src/main/java/com/example/Spring/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/example/Spring/config/auth/dto/OAuthAttributes.java
@@ -27,10 +27,12 @@ public class OAuthAttributes {
     public static OAuthAttributes of(String registrationId,
                                      String userNameAttributeName,
                                      Map<String, Object> attributes) {
+        if("naver".equals(registrationId)){
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
-    //1
     private static OAuthAttributes ofGoogle(String
                                                     userNameAttributeName,
                                             Map<String, Object> attributes) {
@@ -43,7 +45,18 @@ public class OAuthAttributes {
                 .build();
     }
 
-    //2
+    private static OAuthAttributes ofNaver(String userNameAttributeName,
+                                           Map<String, Object> attributes){
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String)response.get("name"))
+                .email((String)response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
     public User toEntity(){
         return User.builder()
                 .name(name)
